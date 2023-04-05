cfdeea5791a4df219a4a1c02b09cc3d05af06a41
sphesihlem <127409749+sphesihlem@users.noreply.github.com>
2023-04-05 20:01:34 +0200

    Update defend.py

--- defend.py
+++ defend.py
@@ -1,8 +1,25 @@
 print("Defensive demo ")
 
 # Numbers 
+global num1
 
 try:
-  num1 = int(input("Enter the 1st number"))
+  num1 = int(input("Enter the 1st number : "))
+  print("This is the number", num1)
 except ValueError:
-  print("Opps that's not a number ")
+  print( " Opps that's not a number ")
+
+except KeyboardInterrupt:
+  print("Done")
+
+# We need to help the user to recover from their mistake 
+
+number = input("Enter a number : ")
+
+# Block until number is a numeric value 
+while number.isnumeric()==False:
+    print(number, "is not a number Try again !!!")
+    number = input("Enter a number : ")
+
+ # Here number will a numeric 
+print(int(number) + 2) 
